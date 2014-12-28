023de65d3f8a0d52b451a17db2076bb9f2fe1a06
bun919tw <bun919tw@gmail.com>
2014-12-28 12:11:34 +0800

    build express server

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var portNum = process.env.PORT || 3000;
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var favicon = require('static-favicon');
+
+
+var app = express();
+
+// app.set('port', portNum);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(portNum, function() {
+  console.log("Listening on " + portNum);
+});
